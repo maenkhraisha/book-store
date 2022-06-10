1d2d62f32a695bc6bbd3af0e54673c2a7285e06f
maen <maenkh@yahoo.com>
2022-06-10 23:22:04 +0300

    add the route link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import './App.css';
-import Header from './components/Header';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import Books from "./Pages/Books";
+import Categories from "./Pages/Categories";
 
 function App() {
   return (
-    <Header />
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route 
+            path="/books" 
+            element={<Books />} 
+            />
+          <Route 
+            path="/categories" 
+            element={<Categories />} 
+          />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 function Header() {
-  <h1>Bookstore </h1>;
+ return( <div>
+<h1>Bookstore (CMS)</h1>
+  <div>
+    <ul>
+      <li><Link to="/books">Books</Link></li>
+      <li><Link to="/categories">Categories</Link></li>
+    </ul>
+  </div>
+ </div>)
 }
 
 export default Header;
