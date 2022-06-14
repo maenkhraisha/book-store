805e2f653b1e9cdf8b633c5c3c830dc9c2640dfe
maen <maenkh@yahoo.com>
2022-06-14 23:56:38 +0300

    add first and second requirments

--- src/Pages/Books.js
+++ src/Pages/Books.js
@@ -2,16 +2,12 @@ import React from 'react';
 import Book from '../components/Book';
 import AddBook from '../components/AddBook';
 
-const bookList = {
-  categorie: 'Action',
-  title: 'The Hunger Games',
-  author: 'Suzanne Collins',
-};
+
 
 function Books() {
   return (
     <div>
-      <Book books={bookList} />
+      <Book />      
       <AddBook />
     </div>
   );
--- src/components/Book.js
+++ src/components/Book.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React from "react";
 
-const Book = (props) => {
-  /* eslint-disable react/prop-types */
-  const { books } = props;
+const bookList = [{
+  key:1,
+  categorie: 'Action',
+  title: 'The Hunger Games',
+  author: 'Suzanne Collins',
+},
+{
+  key:2,
+  categorie: 'Novel',
+  title: 'Ulysses',
+  author: 'James Joyce',
+},
+{
+  key:3,
+  categorie: 'Drama',
+  title: 'Don Quixote',
+  author: 'Herman Melville',
+}];
+
+const Book = () => {
   return (
     <div>
       <ul>
-        <li>
-          <p>{books.categorie}</p>
-          <p>{books.title}</p>
-          <p>{books.author}</p>
-          <button type="button">Comments</button>
-          <button type="button">Remove</button>
-          <button type="button">Edit</button>
-        </li>
+        {bookList.map((b) => (
+          <li key={b.key}>
+            <p>{b.categorie}</p>
+            <p>{b.title}</p>
+            <p>{b.author}</p>
+            <button type="button">Comments</button>
+            <button type="button">Remove</button>
+            <button type="button">Edit</button>
+          </li>
+        ))}
       </ul>
     </div>
   );
